7079defe0ca93accce5c68e0ba248dfe1a7c301e
themuku <murad.mesteliyev3@gmail.com>
2023-06-03 16:45:10 +0400

    Test commit 1

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './auth.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('register')
+  async register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('login/access-token')
+  async getNewTokens(@Body() dto: RefreshTokenDto) {
+    return this.authService.getNewTokens(dto.refreshToken);
+  }
+}
+
+// "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNjg0OTM3ODMzLCJleHAiOjE2ODQ5NDE0MzN9.xIfTEnfmfyOiBwsDhMKNi3TLVsi0aS5IDS830zbIdd8",
+// "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNjg0OTM3ODMzLCJleHAiOjE2ODU1NDI2MzN9.IDINH16_Yjdzvr2Og5lIaNIPCUg8whFPwxUS4X0gegY"
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,117 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { AuthDto } from './auth.dto';
+import { faker } from '@faker-js/faker';
+import { hash, verify } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private configService: ConfigService,
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+    const tokens = await this.issueTokens(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
+
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken, {
+      secret: this.configService.get('JWT_SECRET'),
+    });
+
+    if (!result) throw new UnauthorizedException('Invalid refresh token');
+
+    const user = await this.prisma.user.findUnique({
+      where: { id: result.id },
+    });
+
+    const tokens = await this.issueTokens(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.person.firstName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.phone.number('+994-(##)-(###)-(##)-(##)'),
+        password: await hash(dto.password),
+      },
+    });
+
+    const tokens = await this.issueTokens(user.id);
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens,
+    };
+  }
+
+  private async issueTokens(userId: number) {
+    const data = { id: userId };
+
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+      secret: this.configService.get('JWT_SECRET'),
+    });
+
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '7d',
+      secret: this.configService.get('JWT_SECRET'),
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  private returnUserFields(user: User) {
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
+
+  private async validateUser(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new NotFoundException('User not found');
+
+    const isValid = await verify(user.password, dto.password);
+
+    if (!isValid) throw new UnauthorizedException('Invalid password');
+
+    return user;
+  }
+}
