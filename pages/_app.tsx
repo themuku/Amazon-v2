4efe123c8803a46139b54b2ff59094ba9c312f36
themuku <murad.mesteliyev3@gmail.com>
2023-06-03 16:45:09 +0400

    Test commit 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import AuthProvider from "@/providers/auth-provider/AuthProviders";
+import { TypeComponentAuthFields } from "@/providers/auth-provider/auth-page.types";
+import { persistor, store } from "@/store/store";
+import "@/styles/globals.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import { NextComponentType } from "next/types";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps &
+  TypeComponentAuthFields & {
+    Component: NextComponentType;
+  }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AuthProvider Component={{ isOnlyUser: Component.isOnlyUser }}>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="../public/favicon.ico"
+          sizes="any"
+          type="image/svg+xml"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
